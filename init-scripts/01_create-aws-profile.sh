369b58b75ba35e00941bc30eb5993c4ed17a7572
Raphael Oliveira <raphaeldias.ti@gmail.com>
2022-02-02 18:17:25 -0300

    Update init scripts: remove unnecessary commands

--- init-scripts/01_create-aws-profile.sh
+++ init-scripts/01_create-aws-profile.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 echo "########### Creating profile ###########"
 aws configure set aws_access_key_id localstack_access_key --profile=localstack
 aws configure set aws_secret_access_key localstack_secret_key --profile=localstack
@@ -6,6 +7,3 @@ aws configure set region us-east-1 --profile=localstack
 
 echo "########### Setting default profile ###########"
 export AWS_DEFAULT_PROFILE=localstack
-
-echo "########### Listing profile ###########"
-aws configure list
--- init-scripts/02_create-dynamodb.sh
+++ init-scripts/02_create-dynamodb.sh
@@ -1,17 +1,9 @@
 #!/bin/bash
 
-table_name="Transactions"
-
 echo "########### Create table (Transactions) ###########"
 aws dynamodb create-table \
     --endpoint-url http://localhost:4566 \
-    --table-name ${table_name} \
+    --table-name Transactions \
     --attribute-definitions AttributeName=user,AttributeType=S AttributeName=transaction_id,AttributeType=S \
     --key-schema AttributeName=user,KeyType=HASH AttributeName=transaction_id,KeyType=RANGE \
     --billing-mode PAY_PER_REQUEST
-
-echo "########### Describe table (Transactions) ###########"
-aws dynamodb describe-table \
-    --endpoint-url=http://localhost:4566 \
-    --table-name ${table_name} \
-    --output table
